a2e7749ae2c1925258872e6c4e38ddaa1c09fe07
Erick Perez <eperez@Ericks-MBP.fios-router.home>
2020-06-08 18:34:34 -0400

    Local images added to Platform component and are now displaying

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, {useState, useEffect} from 'react';
+import Header from './components/Header'
 import Game from './components/Game'
 import './App.css';
 import RegF from './components/Registration'
-
+import Platform from './components/Platform'
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Platform />
       <Game />
       <RegF />
     </div>
