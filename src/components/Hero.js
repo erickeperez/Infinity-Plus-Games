f8f90449818ef974ebea18893e35b0ec59321e5c
Erick Perez <eperez@Ericks-MBP.fios-router.home>
2020-06-11 18:57:00 -0400

    Changed Hero component to bootstrap carousel hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,16 +1,40 @@
 import React from 'react'
-import hero3 from '../images/hero3.jpeg'
+import hero4 from '../images/hero4.jpg'
  
 function Hero() {
     return(
-        <div className="heroContainer">
-            <div className="hero">
-                <img src={hero3}  alt="Carousel Banner" />
-                <h2>Welcome to our infinite worlds</h2>
-            </div>
+        <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+        <ol className="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div className="carousel-inner hero"  style={{backgroundImage: `url(${hero4})`}}>
+          <div className="carousel-item active">
+            {/* <img className="d-block w-100" src="..." alt="First slide" style={{backgroundImage: `url(${hero4})`}}> */}
+          </div>
+          <div className="carousel-item">
+            <img className="d-block w-100" src="# " alt="Second slide"/>
+          </div>
+          <div className="carousel-item">
+            <img className="d-block w-100" src="# " alt="Third slide"/>
+          </div>
         </div>
+        <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="sr-only">Previous</span>
+        </a>
+        <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="sr-only">Next</span>
+        </a>
+      </div>
     )
 } 
 
 
+
 export default Hero
+
+
+
