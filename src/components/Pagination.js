c30d14dfdf4026ce4e65a3d255d0d29a0be3a198
unknown <wperez185@yahoo.com>
2020-06-22 19:27:00 -0400

    added function for prev and next btn

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,16 +1,115 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 function Pagination() {
+  // function Paginate(){ 
+    const [currentPage, setCurrentPage] = React.useState(0);
+    const [maxPages, setmaxPages] = React.useState(10);
+
+  function nextPage() {
+     if(currentPage < maxPages){
+       setCurrentPage(prevCurrentPage => prevCurrentPage + 1)
+     }
+     console.log("NEXT ", currentPage)
+   }
+
+     function prevPage() {
+        if(currentPage > 1){
+          setCurrentPage(prevCurrentPage => prevCurrentPage - 1)
+        }
+        console.log("PREV ", currentPage)
+      }
+
+      // useEffect(() => {
+      //   console.log(currentPage);
+      // }, [currentPage])
+    // let maxPages = 4;
+  //   let items = [];
+  //   let leftSide = currentPage - 2;
+  //   if(leftSide <= 0 ) leftSide=1;
+  //   let rightSide = currentPage + 2;
+  //   if(rightSide>maxPages) rightSide = maxPages;
+  //   for (let number = leftSide ; number <= rightSide; number++) {
+  //     items.push(
+  //       <div key={number} className={(number === currentPage ? 'round-effect active' : 'round-effect')} onClick={()=>{ setCurrentPage(number)}}>
+  //         {number}
+  //       </div>,
+  //     );
+  //   }
+  // const nextPage = () => {
+  //   if(currentPage < maxPages){
+  //     setCurrentPage(prevState => prevState + 1)
+  //   }
+  //   console.log(currentPage)
+  // }
+  
+  // const prevPage = () => {
+  //   if(currentPage>1){
+  //     setCurrentPage(currentPage-1)
+  //   }
+  // }
+  
+  //   const paginationRender = (
+  //     <div className="flex-container">
+  //     <div> currentPage : { currentPage } </div>
+        
+  //       <div className="paginate-ctn">
+  //         <div className="round-effect" onClick={prevPage}> &lsaquo; </div>
+  //         {items}
+  //         <div className="round-effect" onClick={nextPage}> &rsaquo; </div>
+  //       </div>
+  //     </div>
+  //   );
+  //   return (paginationRender);
+  // }
+
+
+
+
+  var myHeaders = new Headers();
+  myHeaders.append("user-key", process.env.REACT_APP_GAME_API_KEY);
+  myHeaders.append("Content-Type", "text/plain");
+  myHeaders.append(
+    "Cookie",
+    "__cfduid=de94ed42c783b84caa5238181e82a37651590868499"
+  );
+
+  var raw =
+    "fields id,name,popularity,url,summary,storyline,websites,cover.url; limit 10; ";
+
+  var requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  var proxyUrl = "https://cors-anywhere.herokuapp.com/",
+    targetUrl = "https://api-v3.igdb.com/games";
+
+  fetch(proxyUrl + targetUrl, requestOptions)
+    .then((blob) => blob.json())
+    .then((data) => {
+      data.forEach((r) => {
+      
+      });
+      return data;
+    })
+    .catch((e) => {
+      console.log(e);
+      return e;
+    });
+
+
   return (
     <div>
       <nav aria-label="Page navigation example">
         <ul className="pagination pagination-lg justify-content-center">
-          <li className="page-item disabled">
+          <li className="page-item">
             <a
               className="page-link"
               href="/#"
               tabIndex="-1"
-              aria-disabled="true"
+              onClick={prevPage}
             >
               Previous
             </a>
@@ -29,9 +128,9 @@ function Pagination() {
             <a className="page-link" href="/#">
               3
             </a>
-          </li>
+          </li> 
           <li className="page-item">
-            <a className="page-link" href="/#">
+            <a className="page-link" href="/#" onClick={nextPage}>
               Next
             </a>
           </li>
