10a7f2bc6973ceb11c4ad5e2dafaa7de2b3f704d
unknown <wperez185@yahoo.com>
2020-06-23 21:41:02 -0400

    finished carousel

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,58 +1,42 @@
-import React, {useState} from 'react'
-import { Carousel } from 'react-bootstrap';
- 
+import React, { useState, useEffect } from "react";
+import { Carousel } from "react-bootstrap";
+
 function Hero() {
-  // const [startIndex, setStartIndex] = useState(0);
+  const [heroImg, setHeroImg] = useState([])
 
-  // const handleSelect = (selectedIndex, e) => {
-  //   setStartIndex(1);
-  // };
+  useEffect(() => {
+    fetch(`https://newsapi.org/v2/top-headlines?sources=ign&pageSize=3&apiKey=${process.env.REACT_APP_HERO_API_KEY}`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setHeroImg(result.articles);
+        },
+       
+        (error) => {
+        }
+      )
+  }, [])
 
-
-  fetch(`https://newsapi.org/v2/top-headlines?sources=ign&pageSize=3&apiKey=${process.env.REACT_APP_HERO_API_KEY}`)
-    .then((blob) => blob.json())
-    .then((data) => {
-      data.articles.map((r, index) => {
-        var carouselBody = document.getElementById("carousel-inner");
-        carouselBody.insertAdjacentHTML(
-          "afterbegin",
-          index === 0 ?
-         `<div class="carousel-item active carousel-${index}">
-          <img class="d-block w-100" src="${r.urlToImage}" alt="Game Slide"/>
-        </div>` :
-        `<div class="carousel-item carousel-${index}">
-        <img class="d-block w-100" src="${r.urlToImage}" alt="Game Slide"/>
-      </div>`
-      
-        );
-      });
-      // data.articles.forEach((r, index) => {
-      // //   // console.log(index)
-      //   var carouselBody = document.getElementById("carousel-inner");
-      //   carouselBody.insertAdjacentHTML(
-      //     "afterbegin",
-      //    `<div class="carousel-item active ${index}">
-      //     <img class="d-block w-100" src="${r.urlToImage}" alt="Game Slide"/>
-      //   </div>` 
-      //   );
-      // });
-      return data;
-    })
-    .catch((e) => {
-      console.log(e);
-      return e;
-    });
-
-    return (
-    <Carousel id="carousel-inner" >
-  
-</Carousel>
-
+  const listItems = heroImg.map((arr, index) =>
+  <Carousel.Item> 
+    <div>
+    <img
+          className="d-block w-100"
+          key={index}
+          src={arr.urlToImage}
+          alt="First slide"
+        />
+    </div>
+  </Carousel.Item>
   );
+  
+  return (
+  
+    <Carousel>
+    {listItems}
+     
+    </Carousel>
+  );
+}
 
-} 
-
-export default Hero
-
-
-
+export default Hero;
